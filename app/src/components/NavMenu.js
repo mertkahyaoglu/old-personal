c1e00c2aad78ac19e05f579def700189731bba11
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2017-02-25 15:52:54 +0200

    organize styles

--- app/src/components/NavMenu.js
+++ app/src/components/NavMenu.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const NavMenu = () => (
   <ul>
+    <li><a href="#about">About me</a></li>
+    <li><a href="#skills">Skills</a></li>
     <li><a href="#portfolio">Portfolio</a></li>
     <li><a href="http://mert-kahyaoglu.com/blog/">Blog</a></li>
   </ul>
