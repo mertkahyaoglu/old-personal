9ddd0c2a2d152356e17c959872610cdc572cc05c
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2017-02-25 15:50:52 +0200

    classify skills

--- app/data/skills.js
+++ app/data/skills.js
@@ -1,17 +1,74 @@
 var skills = {
-  "java": "img/logos/java.png",
-  "python": "img/logos/python.png",
-  "javascript": "img/logos/javascript.png",
-  "nodejs": "img/logos/nodejs.png",
-  "reactjs": "img/logos/reactjs.png",
-  "angular": "img/logos/angularjs.png",
-  "coffeescript": "img/logos/coffeescript.png",
-  "android": "img/logos/android.png",
-  "laravel": "img/logos/laravel.png",
-  "html": "img/logos/html5.png",
-  "css": "img/logos/css3.png",
-  "mysql": "img/logos/mysql.png",
-  "mongodb": "img/logos/mongodb.png",
-  "git": "img/logos/git.png",
-  "libgdx": "img/logos/libgdx.png"
+  languages: [
+    {
+      name: "javascript",
+      image: "images/logos/js.png"
+    },
+    {
+      name: "python",
+      image: "images/logos/py.png"
+    },
+    {
+      name: "php",
+      image: "images/logos/php.png"
+    },
+    {
+      name: "ruby",
+      image: "images/logos/ruby.png"
+    },
+    {
+      name: "java",
+      image: "images/logos/java.png"
+    },
+    {
+      name: "go",
+      image: "images/logos/go.png"
+    }
+  ],
+  technologies: [
+    {
+      name: "nodejs",
+      image: "images/logos/node.png"
+    },
+    {
+      name: "react",
+      image: "images/logos/react.png"
+    },
+    {
+      name: "angular",
+      image: "images/logos/angular.png"
+    },
+    {
+      name: "coffeescript",
+      image: "images/logos/coffee.png"
+    },
+    {
+      name: "laravel",
+      image: "images/logos/laravel.png"
+    },
+    {
+      name: "android",
+      image: "images/logos/android.png"
+    },
+    {
+      name: "git",
+      image: "images/logos/git.png"
+    }
+  ],
+  storage: [
+    {
+      name: "mysql",
+      image: "images/logos/mysql.png"
+    },
+    {
+      name: "mongodb",
+      image: "images/logos/mongo.png"
+    },
+    {
+      name: "redis",
+      image: "images/logos/redis.png"
+    }
+  ]
 }
+
+module.exports = skills
