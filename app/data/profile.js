341daf3da8ab2cdb4a3d5208c2ecd878761498a5
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2017-09-20 21:09:34 +0300

    info update

--- app/data/profile.js
+++ app/data/profile.js
@@ -2,7 +2,7 @@ var profile = {
   name: "Mert Kahyaoğlu",
   title: "Software Engineer",
   bio: {
-    __html: "<p>I am Mert Kahyaoğlu, a software engineer living in Marmaris, Turkey.</p><p>I love improving my coding skills, writing open source code and contributing to projects on GitHub. When I find some free time, I try to share my knowledge by writing blog (not so often these days).</p><p>I enjoy trying out new technologies and creating projects with them. You can find all of my projects here. They all are open source waiting for you to look at :)</p>"
+    __html: '<p>Hi! I\'m Mert, a software engineer working at <a href="http://iyzico.com/" class="iyzico" target="_blank">iyzico</a>.</p><p>I\'m a big fan of continuous learning. I always try to discover what is new. I love improving my coding skills, writing open source code and contributing to projects on GitHub. When I find some free time, I try to share my knowledge by writing blog.</p><p>I enjoy trying out new technologies and creating projects with them. You can find all of my projects here. They all are open source waiting for you to look at :)</p>'
   },
   links: {
     github: "https://github.com/mertkahyaoglu",
