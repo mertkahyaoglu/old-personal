a82098aa1db4934caedafc3d8b5a54b58d7420fb
mert.kahyaoglu <mert.kahyaoglu@iyzico.com>
2018-08-20 20:14:58 +0300

    simplify

--- app/src/index.js
+++ app/src/index.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
-import configureStore from './store/configureStore';
-import { syncHistoryWithStore } from 'react-router-redux';
-import register from './registerServiceWorker';
 
-import App from './App';
 import Home from './containers/Home';
 
 import './styles/index';
 
-const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
-
-render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route component={App}>
-        <Route path="/" component={Home} />
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
-
-register();
+render(<Home />, document.getElementById('root'));
